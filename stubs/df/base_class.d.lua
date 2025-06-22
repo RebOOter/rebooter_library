2f7edac776f5e7840348882a28b4633d36543be2
rebooter <rebooter8d@gmail.com>
2025-06-22 14:22:55 +0200

    Fixed base class is_instance

--- stubs/df/base_class.d.lua
+++ stubs/df/base_class.d.lua
@@ -2,5 +2,6 @@
 ---@class df.base_class
 local base_class = {}
 
+---@param object df.base_class
 ---@return boolean
-function base_class:is_instance() end
+function base_class:is_instance(object) end
