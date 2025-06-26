0a5849f13eb93d3a93340c302d9eafa914166d19
rebooter <rebooter8d@gmail.com>
2025-06-26 22:24:16 +0200

    Added a few functions to the burrows

--- stubs/dfhack/burrows.d.lua
+++ stubs/dfhack/burrows.d.lua
@@ -3,5 +3,15 @@
 burrows = {}
 
 ---@param burrow df.burrow
+---@param unit df.unit
+---@return boolean
+function burrows.isAssignedUnit(burrow, unit) end
+
+---@param burrow df.burrow
+---@param unit df.unit
+---@param enable boolean
+function burrows.setAssignedUnit(burrow, unit, enable) end
+
+---@param burrow df.burrow
 ---@return df.map_block[]
 function burrows.listBlocks(burrow) end
